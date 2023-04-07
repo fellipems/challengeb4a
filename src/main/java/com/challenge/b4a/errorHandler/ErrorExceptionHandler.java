1ddcb92478176e32eab6cc51d0a435e0fe81aee7
fellipe <fellipems14@gmail.com>
2023-04-07 18:46:48 -0300

    add exceptionHandler ao nao informar dados obrigatorios na Excecao DadoObrigatorioNaoInformadoException

--- src/main/java/com/challenge/b4a/errorHandler/ErrorExceptionHandler.java
+++ src/main/java/com/challenge/b4a/errorHandler/ErrorExceptionHandler.java
@@ -1,9 +1,27 @@
 package com.challenge.b4a.errorHandler;
 
+import com.challenge.b4a.exceptions.DadoObrigatorioNaoInformadoException;
+import com.challenge.b4a.resources.exceptions.StandardError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
 @ControllerAdvice
-public class ExceptionHandler {
-    @org.springframework.web.bind.annotation.ExceptionHandler()
+public class ErrorExceptionHandler {
+
+    @ExceptionHandler(DadoObrigatorioNaoInformadoException.class)
+    public ResponseEntity<?> handleDadoObrigatorioNaoInformadoException(DadoObrigatorioNaoInformadoException exception, HttpServletRequest request) {
+        StandardError standardErrorBuilder = StandardError.Builder
+                .newBuilder()
+                .withTimestamp(new Date().getTime())
+                .withStatus(HttpStatus.BAD_REQUEST.value())
+                .withMessage(exception.getMessage())
+                .withPath(request.getRequestURI()).build();
+
+        return new ResponseEntity<>(standardErrorBuilder, HttpStatus.BAD_REQUEST);
+    }
 }
