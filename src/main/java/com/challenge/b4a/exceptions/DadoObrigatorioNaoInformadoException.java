0d9ef3a300370b518f78c3fe079dc60a437b2ba2
fellipe <fellipems14@gmail.com>
2023-04-07 18:47:18 -0300

    style: altera nome package

--- src/main/java/com/challenge/b4a/exceptions/DadoObrigatorioNaoInformadoException.java
+++ src/main/java/com/challenge/b4a/exceptions/DadoObrigatorioNaoInformadoException.java
@@ -1,4 +1,4 @@
-package com.challenge.b4a.exception;
+package com.challenge.b4a.exceptions;
 
 public class DadoObrigatorioNaoInformadoException extends RuntimeException {
     public DadoObrigatorioNaoInformadoException() {
