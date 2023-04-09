fe65e18b09021d6bf7e16a4c5c05fc2c4dfa77c0
fellipe <fellipems14@gmail.com>
2023-04-09 02:45:18 -0300

    test: add testes da classe controller do usuario, add displayName nos testes

--- src/test/java/com/challenge/b4a/UsuarioControllerTest.java
+++ src/test/java/com/challenge/b4a/UsuarioControllerTest.java
@@ -1,107 +1,94 @@
 package com.challenge.b4a;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.when;
-
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import com.challenge.b4a.controllers.UsuarioController;
 import com.challenge.b4a.domains.Usuario;
 import com.challenge.b4a.services.UsuarioService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-public class UsuarioServiceTest {
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class UsuarioControllerTest {
 
     private UsuarioController usuarioController;
+
+    Long usuarioId = 1L;
 
     @Mock
     private UsuarioService usuarioService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this);
         usuarioController = new UsuarioController(usuarioService);
     }
 
     @Test
-    void criaUsuario_deveRetornarUsuarioCriado() {
-        // given
+    @DisplayName("Teste criaUsuario - deve retornar um ResponseEntity com status CREATED e o usuário criado")
+    void deveCriarUsuario() {
         Usuario usuario = new Usuario();
         when(usuarioService.criaUsuario(any(Usuario.class))).thenReturn(usuario);
 
-        // when
         ResponseEntity<Usuario> response = usuarioController.criaUsuario(usuario);
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.CREATED);
         assertThat(response.getBody()).isEqualTo(usuario);
     }
 
     @Test
-    void atualizaUsuario_deveRetornarUsuarioAtualizado() {
-        // given
-        Long usuarioId = 1L;
+    @DisplayName("Teste atualizaUsuario - deve retornar um ResponseEntity com status OK e o usuário atualizado")
+    void deveAtualizarERetornarUsuarioAtualizado() {
         Usuario usuarioAtualizado = new Usuario();
         when(usuarioService.atualizaUsuario(anyLong(), any(Usuario.class))).thenReturn(usuarioAtualizado);
 
-        // when
         ResponseEntity<Usuario> response = usuarioController.atualizaUsuario(usuarioId, usuarioAtualizado);
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(usuarioAtualizado);
     }
 
     @Test
-    void deletaUsuario_deveRetornarNoContent() {
-        // given
-        Long usuarioId = 1L;
-
-        // when
+    @DisplayName("Teste deletaUsuario - deve retornar um ResponseEntity com status NO_CONTENT e o usuário removido")
+    void deveDeletarUsuarioERetornarUmNoContent() {
         ResponseEntity<Void> response = usuarioController.deletaUsuario(usuarioId);
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
         assertThat(response.getBody()).isNull();
     }
 
     @Test
-    void listaUsuarios_deveRetornarListaDeUsuarios() {
-        // given
+    @DisplayName("Teste listaUsuarios - deve retornar um ResponseEntity com status OK e lista de usuários")
+    void deveListarTodosUsuarios() {
         List<Usuario> usuarios = Arrays.asList(new Usuario(), new Usuario());
         when(usuarioService.listaTodosUsuarios()).thenReturn(usuarios);
 
-        // when
         ResponseEntity<List<Usuario>> response = usuarioController.listaUsuarios();
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(usuarios);
     }
 
     @Test
-    void listaUsuarioById_deveRetornarUsuarioComId() {
-        // given
-        Long usuarioId = 1L;
+    @DisplayName("Teste listaUsuarioById - deve retornar um ResponseEntity com status OK e usuário com Id informado")
+    void deveListarUsuarioComIdInformado() {
         Usuario usuario = new Usuario();
         when(usuarioService.listaUsuarioById(usuarioId)).thenReturn(usuario);
 
-        // when
         ResponseEntity<Usuario> response = usuarioController.listaUsuarioById(usuarioId);
 
-        // then
         assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
         assertThat(response.getBody()).isEqualTo(usuario);
     }
-
 }
