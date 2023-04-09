32110bca992a4ad46e49a28641bba1bf057715a0
fellipe <fellipems14@gmail.com>
2023-04-09 19:44:24 -0300

    feat: add classe Mensages para consumir nosso bundle messages_pt com mensagens atualizadas e personalizaveis

--- src/main/java/com/challenge/b4a/utils/Mensagens.java
+++ src/main/java/com/challenge/b4a/utils/Mensagens.java
@@ -1,2 +1,9 @@
-package com.challenge.b4a.utils;public class Messages {
+package com.challenge.b4a.utils;
+
+import java.util.ResourceBundle;
+
+public class Mensagens {
+    public static String usar(String idMensagem) {
+        return ResourceBundle.getBundle("messages_pt").getString(idMensagem);
+    }
 }
