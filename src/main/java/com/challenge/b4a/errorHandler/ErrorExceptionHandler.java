b7577363e395353092af7ba24b2b298af2bccf39
fellipe <fellipems14@gmail.com>
2023-04-09 02:40:18 -0300

    feat: add dois novos ExceptionHandlers de endereco nao encontrado e erro de parse do JSON

--- src/main/java/com/challenge/b4a/errorHandler/ErrorExceptionHandler.java
+++ src/main/java/com/challenge/b4a/errorHandler/ErrorExceptionHandler.java
@@ -1,11 +1,13 @@
 package com.challenge.b4a.errorHandler;
 
 import com.challenge.b4a.exceptions.DadoObrigatorioNaoInformadoException;
+import com.challenge.b4a.exceptions.EnderecoNaoEncontradoException;
 import com.challenge.b4a.exceptions.UsuarioNaoEncontradoException;
 import com.challenge.b4a.exceptions.UsuarioNaoInformadoException;
 import com.challenge.b4a.resources.exceptions.StandardError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -21,7 +23,21 @@ public class ErrorExceptionHandler {
                 .newBuilder()
                 .withTimestamp(new Date().getTime())
                 .withStatus(HttpStatus.NOT_FOUND.value())
-                .withMessage("Ocorreu um erro inesperado, tente novamente mais tarde")
+                .withMessage("Ocorreu um erro inesperado, tente novamente mais tarde " + exception.getMessage())
+                .withError(exception.getClass().getCanonicalName())
+                .withPath(request.getRequestURI()).build();
+
+        return new ResponseEntity<>(standardErrorBuilder, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception, HttpServletRequest request) {
+        StandardError standardErrorBuilder = StandardError.Builder
+                .newBuilder()
+                .withTimestamp(new Date().getTime())
+                .withStatus(HttpStatus.NOT_FOUND.value())
+                .withMessage("Não é possível converter texto no campo número")
+                .withError(exception.getClass().getCanonicalName())
                 .withPath(request.getRequestURI()).build();
 
         return new ResponseEntity<>(standardErrorBuilder, HttpStatus.NOT_FOUND);
@@ -53,6 +69,18 @@ public class ErrorExceptionHandler {
 
     @ExceptionHandler(UsuarioNaoEncontradoException.class)
     public ResponseEntity<?> handleUsuarioNaoEncontradoException(UsuarioNaoEncontradoException exception, HttpServletRequest request) {
+        StandardError standardErrorBuilder = StandardError.Builder
+                .newBuilder()
+                .withTimestamp(new Date().getTime())
+                .withStatus(HttpStatus.NOT_FOUND.value())
+                .withMessage(exception.getMessage())
+                .withPath(request.getRequestURI()).build();
+
+        return new ResponseEntity<>(standardErrorBuilder, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EnderecoNaoEncontradoException.class)
+    public ResponseEntity<?> handleEnderecoNaoEncontradoException(EnderecoNaoEncontradoException exception, HttpServletRequest request) {
         StandardError standardErrorBuilder = StandardError.Builder
                 .newBuilder()
                 .withTimestamp(new Date().getTime())
